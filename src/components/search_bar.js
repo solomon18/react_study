ae94e91408131a233e9184f9309cecfe8fc4fdcb
Joseph (Changgon) Cho <4066364+solomon18@users.noreply.github.com>
2018-08-17 17:43:44 +0900

    Add react wrapup

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -17,17 +17,18 @@ class SearchBar extends Component {
   render() {
     // return <input onChange={this.onInputChange} />;
     return (
-      <div>
+      <div className="search-bar">
         <input 
           value={this.state.term}
-          onChange={(event) => this.setState({ term: event.target.value }) }/>
+          onChange={(event) => this.onInputChange(event.target.value) }/>
       </div>
     );
   }
 
-  // onInputChange(event) {
-  //   console.log(event.target.value);
-  // }
+  onInputChange(term) {
+    this.setState ({term});
+    this.props.onSearchTermChange(term);
+  }
 }
 
 export default SearchBar;
