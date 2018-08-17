1e41c984d88713d8a213bc41c5470a4ba00cbe13
Joseph (Changgon) Cho <4066364+solomon18@users.noreply.github.com>
2018-08-17 16:13:31 +0900

    Add video-list

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,12 +10,19 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
+    // only inside constructor
     this.state = { term: '' };
   }
 
   render() {
     // return <input onChange={this.onInputChange} />;
-    return <input onChange={(event) => console.log(event.target.value) }/>;
+    return (
+      <div>
+        <input 
+          value={this.state.term}
+          onChange={(event) => this.setState({ term: event.target.value }) }/>
+      </div>
+    );
   }
 
   // onInputChange(event) {
