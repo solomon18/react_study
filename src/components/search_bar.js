ff27540d23c6b9cbf4fb9470b8fb2d9d5f3d7f7b
Joseph (Changgon) Cho <4066364+solomon18@users.noreply.github.com>
2018-08-17 11:51:10 +0900

    Fix search_bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+// const Component = React.Component;
 
-const SearchBar = () => {
-  return <input />; // React.createElement
-};
+
+// const SearchBar = () => {
+//   return <input />; // React.createElement
+// };
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    // return <input onChange={this.onInputChange} />;
+    return <input onChange={(event) => console.log(event.target.value) }/>;
+  }
+
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+}
 
 export default SearchBar;
